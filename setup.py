081dab292c2b434ff31ad8396679a9e1667127f3
AayushGupta16 <aayugupta04@gmail.com>
2025-03-02 14:34:39 -0500

    Bump version to 0.1.32

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beautiful-captions",
-    version="0.1.30",
+    version="0.1.32",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
