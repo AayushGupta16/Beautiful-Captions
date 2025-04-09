2e42bb551a04b7a9437f826426e956e9e0854002
AayushGupta16 <aayugupta04@gmail.com>
2025-04-09 03:10:17 -0400

    Bump version to 0.1.62

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beautiful-captions",
-    version="0.1.61",
+    version="0.1.62",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
--- src/beautiful_captions/__init__.py
+++ src/beautiful_captions/__init__.py
@@ -7,7 +7,7 @@ from .styling.style import StyleManager, FontManager
 from .styling.animation import AnimationFactory, create_animation_for_subtitle
 from .utils.subtitles import style_srt_content
 
-__version__ = "0.1.61"
+__version__ = "0.1.62"
 
 __all__ = [
     # Main functions
