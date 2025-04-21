7ba86fcb49fd4b36c8aa19cfae996e7d234f1f25
echen <eschen003@gmail.com>
2025-04-21 17:26:16 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beautiful-captions",
-    version="0.1.62",
+    version="0.1.63",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
