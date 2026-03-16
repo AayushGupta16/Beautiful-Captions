7bacb5e619fdd5f75726da85bdf82e35aa54a25b
Eric Chen <eschen003@gmail.com>
2026-03-16 19:14:55 +0100

    universal-2

--- src/beautiful_captions/transcription/assemblyai.py
+++ src/beautiful_captions/transcription/assemblyai.py
@@ -4,7 +4,6 @@ from typing import List, Optional, Dict
 import assemblyai as aai
 from datetime import timedelta
 import asyncio
-from assemblyai import SpeechModel
 from .base import TranscriptionService, Utterance, Word
 
 logger = logging.getLogger(__name__)
@@ -41,7 +40,7 @@ class AssemblyAIService(TranscriptionService):
         logger.info(f"Audio file size: {os.path.getsize(audio_path)} bytes")
         
         config = aai.TranscriptionConfig(
-            speech_model=SpeechModel.nano,
+            speech_models=["universal-2"],
             speaker_labels=True,
             speakers_expected=max_speakers,
             language_detection=True,
